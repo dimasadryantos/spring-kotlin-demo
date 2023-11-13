ac62cab7a6b20f8163e9417f7d83782bad4112e3
dimassetyawan <dimasadriyantos@gmail.com>
2023-11-13 18:14:18 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/springkotlindemo/SpringKotlinDemoApplication.kt
@@ -0,0 +1,15 @@
+package com.example.springkotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinDemoApplication>(*args)
+}
+
+fun `when`(): String {
+    return ""
+}
